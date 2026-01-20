65d960bd5ab0b80ae21fa2ae6de5d6fc918c42e0
update_mgr <update_mgr@update-mgrs-MacBook-Pro-5.local>
2026-01-19 23:54:49 -0500

    Add Vercel Speed Insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -28,7 +29,8 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
-      </body>
+        <SpeedInsights />
+    </body>
     </html>
   );
 }
